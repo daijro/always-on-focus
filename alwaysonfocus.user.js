106714a3e4f3a2b895dafd10e806939acfe87198
daijro <72637910+daijro@users.noreply.github.com>
2023-04-21 08:18:20 -0500

    Exclude anchor and span elements, add update URL

--- alwaysonfocus.user.js
+++ alwaysonfocus.user.js
@@ -2,9 +2,11 @@
 // @name          Always on focus
 // @namespace     https://github.com/daijro/always-on-focus
 // @author        daijro
-// @version       1.3
+// @version       1.4
 // @description   Prevents websites from knowing that you switched tabs or unfocused the window
 // @include       *
+// @updateURL     https://github.com/daijro/always-on-focus/raw/main/alwaysonfocus.user.js
+// @downloadURL   https://github.com/daijro/always-on-focus/raw/main/alwaysonfocus.user.js
 // @run-at        document-start
 // ==/UserScript==
 
@@ -32,8 +34,11 @@ unsafeWindow.document.onvisibilitychange = undefined;
 
 
 var event_handler = (event) => {
-    if (["blur", "mouseleave", "mouseout"].includes(event.type) && event.target instanceof HTMLInputElement) {
-        return // exclude input elements
+    if (["blur", "mouseleave", "mouseout"].includes(event.type) &&
+        (event.target instanceof HTMLInputElement ||
+         event.target instanceof HTMLAnchorElement ||
+         event.target instanceof HTMLSpanElement)) {
+        return // exclude input, anchor, and span elements
     }
     event.preventDefault();
     event.stopPropagation();
