e70463149823089974334e8a553d558acd34d6dd
daijro <72637910+daijro@users.noreply.github.com>
2023-10-09 20:29:52 -0500

    Fix #4 v1.5.1

--- alwaysonfocus.user.js
+++ alwaysonfocus.user.js
@@ -2,7 +2,7 @@
 // @name          Always on focus
 // @namespace     https://github.com/daijro/always-on-focus
 // @author        daijro
-// @version       1.5
+// @version       1.5.1
 // @description   Prevents websites from knowing that you switched tabs or unfocused the window
 // @include       *
 // @updateURL     https://github.com/daijro/always-on-focus/raw/main/alwaysonfocus.user.js
@@ -33,12 +33,35 @@ Object.defineProperty(document, "webkitVisibilityState", {get: () => "visible"})
 unsafeWindow.document.onvisibilitychange = undefined;
 
 
+// element constructors to allow blur events on
+const blurWhitelist = [
+    HTMLInputElement,
+    HTMLAnchorElement,
+    HTMLSpanElement,
+    HTMLParagraphElement,
+]
+
+// element constructors to block mouseleave and mouseout events on
+const hoverBlacklist = [
+    HTMLIFrameElement,
+    HTMLHtmlElement,
+    HTMLBodyElement,
+    HTMLHeadElement,
+    HTMLFrameSetElement, // obsolete but included for completeness
+    HTMLFrameElement // obsolete but included for completeness
+];
+
 var event_handler = (event) => {
-    if (["blur", "mouseleave", "mouseout"].includes(event.type) &&
-        (event.target instanceof HTMLInputElement ||
-         event.target instanceof HTMLAnchorElement ||
-         event.target instanceof HTMLSpanElement)) {
-        return // exclude input, anchor, and span elements
+    // if the event is blur, and the target is an whitelisted type, allow it
+    if (event.type === 'blur'
+        && blurWhitelist.some(type => event.target instanceof type)
+        || event.target.classList.contains('ql-editor')) { // quill js fix
+        return;
+    }
+    // if the event is mouseleave or mouseout, and the target is an blacklisted type, block it
+    if (['mouseleave', 'mouseout'].includes(event.type)
+        && !hoverBlacklist.some(type => event.target instanceof type)) {
+        return;
     }
     event.preventDefault();
     event.stopPropagation();
